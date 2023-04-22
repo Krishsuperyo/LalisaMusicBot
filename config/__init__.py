112c6ffc87f2c883f34fcc41687add66a5ccd0e0
ᴋʀɪsʜsᴜᴘᴇʀʏᴏ <131173834+Krishsuperyo@users.noreply.github.com>
2023-04-22 15:11:46 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2023-2024 by Krishsuperyo@Github, < https://github.com/Krishsuperyo >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/Krishsuperyo/LalisaMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/Krishsuperyo/LalisaMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
